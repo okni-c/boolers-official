2a36ff69511e4ee270e880a2dfbe57158a7e5b71
okni-c <oknistay@gmail.com>
2021-08-10 03:50:54 -0500

    added session and cart functionality

--- routes/index.js
+++ routes/index.js
@@ -36,52 +36,52 @@ router.get('/stories', (req, res) => {
 });
 
 // user stuff
-router.get('/signup', (req, res) => {
-    var messages = req.flash('error');
-    res.render('signup', { layout: 'index', title: 'Boolers Official - Sign Up', csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
-});
-
-router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/merch',
-    failureRedirect: '/signup',
-    failureFlash: true
-}));
-
-router.get('/signin', (req, res) => {
-    var messages = req.flash('error');
-    res.render('signin', { layout: 'index', title: 'Boolers Official - Sign In', csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
-});
-
-router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/merch',
-    failureRedirect: '/signin',
-    failureFlash: true
-}));
-
-router.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile', { layout: 'index', title: 'Boolers Official - Profile' });
-});
-
-router.get('/logout', function(req, res, next) {
-    req.logout();
-    res.redirect('/');
-});
-
-// // cart stuff
-// router.get('/add-to-cart:id', (req, res) => {
-//     var productId = req.params.id;
-//     var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
-
-//     Product.findById(productId, function(err, product) {
-//         if (err) {
-//             return res.redirect('/');
-//         }
-//         cart.add(product, product.id);
-//         req.session.cart = cart;
-//         console.log(req.session.cart);
-//         req.redirect('/merch');
-//     });
+// router.get('/signup', (req, res) => {
+//     var messages = req.flash('error');
+//     res.render('signup', { layout: 'index', title: 'Boolers Official - Sign Up', csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 // });
+
+// router.post('/signup', passport.authenticate('local.signup', {
+//     successRedirect: '/merch',
+//     failureRedirect: '/signup',
+//     failureFlash: true
+// }));
+
+// router.get('/signin', (req, res) => {
+//     var messages = req.flash('error');
+//     res.render('signin', { layout: 'index', title: 'Boolers Official - Sign In', csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
+// });
+
+// router.post('/signin', passport.authenticate('local.signin', {
+//     successRedirect: '/merch',
+//     failureRedirect: '/signin',
+//     failureFlash: true
+// }));
+
+// router.get('/profile', isLoggedIn, function(req, res) {
+//     res.render('profile', { layout: 'index', title: 'Boolers Official - Profile' });
+// });
+
+// router.get('/logout', function(req, res, next) {
+//     req.logout();
+//     res.redirect('/');
+// });
+
+// cart stuff
+router.get('/add-to-cart/:id', (req, res) => {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    Product.findById(productId, function(err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/merch');
+    });
+});
 
 router.use((req, res) => {
     res.status(404).send('<h1>404 Error: This page does not exist.</h1> <h4> Please try a different address route.</h4>');
@@ -89,9 +89,9 @@ router.use((req, res) => {
 
 module.exports = router;
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/');
-};
+// function isLoggedIn(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     }
+//     res.redirect('/');
+// };
--- server.js
+++ server.js
@@ -6,30 +6,44 @@ const session = require('express-session');
 const passport = require('passport');
 const flash = require('connect-flash');
 const validator = require('express-validator');
+var MongoStore = require('connect-mongo');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const stripe = require('stripe')(process.env.STRIPE_KEY);
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-app.use(validator());
-app.use(session({secret: 'supersecretsecret', resave: false, saveUninitialized: false}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-require('./config/passport');
-
-app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({partialsDir: __dirname + '/views/partials/'}));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/boolers-official', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(validator());
+app.use(session({
+    secret: 'supersecretsecret',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/boolers-official',
+      }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport');
+
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+})
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({partialsDir: __dirname + '/views/partials/'}));
+
 
 // log mongo queries ;)
 mongoose.set('debug', true);
